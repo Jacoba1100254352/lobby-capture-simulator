d6448ef2d1b890a9be043c10d05382ee3012aba7
Jacob Anderson <jacobdanderson@gmail.com>
2026-06-05 18:12:46 -0400

    Fix CI release tag gate and Java test entrypoint

--- src/test/java/lobbycapture/SimulatorTests.java
+++ src/test/java/lobbycapture/SimulatorTests.java
@@ -9,7 +9,7 @@ public final class SimulatorTests
 	private SimulatorTests() {
 	}
 	
-	static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		AdaptationTest.run();
 		SmokeTest.run();
 		System.out.println("All simulator tests passed.");
